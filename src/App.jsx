7dd1ddf0a15c8da681f969ddc6ab7e4d1d40acaa
afsane <afsanerasoulibahador@gmail.com>
2024-12-19 17:44:31 +0330

    feat: add CommentList Components for rendering Comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css';
+import { useComment } from "./comments/useComment";
+import CommentList from "./comments/CommentList";
 
 function App() {
+  const { error, isLoading, comments, addReply } = useComment();
+  if (error) return <p>error: {error}</p>;
+  if (isLoading) return <p>Loading ....</p>;
   return (
     <div>
-      
+      <CommentList comments={comments} addReply={addReply} />
     </div>
   );
 }
---
+++ src/comments/CommentList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Comment from "./Comment";
+function CommentList({ comments, addReply }) {
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} addReply={addReply} />
+      ))}
+    </div>
+  );
+}
+
+export default CommentList;
